9e3a265b4becf669df19d7a2608e526643f14924
Romeo <popescuromeo22@stud.ase.ro>
2024-08-06 16:48:32 +0300

    Finished phonebook step 6 from part3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-
-const persons = [
+app.use(express.json())
+let persons = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -35,6 +35,52 @@ app.get('/info', (request, response) => {
   const len = persons.length
   const date = new Date().toString()
   response.send(`<p>Phonebook has info for ${len} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  response.json(person)
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const rand = Math.random() * 100
+  const maxid = parseInt(String(rand), 10)
+
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  else if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'the name already exists in phonebook'
+    })
+  }
+
+  const person = {
+    id: String(maxid), 
+    name: body.name,
+    number: body.number,
+  } 
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 const PORT = 3001
 app.listen(PORT)
