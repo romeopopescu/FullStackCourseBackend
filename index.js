6dd1afd218d5b88c192322b6c70cd34a9a99c082
Romeo <popescuromeo22@stud.ase.ro>
2024-08-08 15:16:02 +0300

    Added morgan middleware and updated the function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -23,20 +24,19 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
-const requestLogger = (request, response, next) => {
-  console.log('Method: ', request.method)  
-  console.log('Path: ', request.path)
-  console.log('Body: ', request.body)
-  console.log('---')
-  next()
-}
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})
 
 app.use(express.json())
-app.use(requestLogger)
+app.use(morgan('combined'))
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
 app.get('/', (request, response) => {
   response.send('<h1>Hello world!</h1>')
 })
@@ -97,7 +97,6 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
